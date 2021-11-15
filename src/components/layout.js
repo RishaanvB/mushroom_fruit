9fc64f1e7f020b70b48988a011c7a93237f0c853
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-11-15 21:53:08 +0100

    rename importing footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useEffect } from "react"
 import "./layout.css"
 import MainHeader from "./mainheader"
-import Footer from "./Footer"
+import Footer from "./footer"
 
 const Layout = ({ children, pageTitle }) => {
   return (
