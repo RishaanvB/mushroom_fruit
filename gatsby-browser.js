3c4e64fe7082f04a1961493d62ae7495d1462546
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-11-28 21:46:53 +0100

    add font poppins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+
+// font import here according to https://www.gatsbyjs.com/docs/how-to/styling/using-web-fonts/
+
+import "@fontsource/poppins" // Defaults to weight 400 with all styles included.
