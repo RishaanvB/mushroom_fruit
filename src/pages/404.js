43ea2ff653fd7748d550d432ad6cab400b07ea17
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-11-29 12:58:58 +0100

    add 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,37 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import LayoutBackground from "../styles/LayoutBackground"
+import { Container, Button } from "../styles/CustomStyles"
+import background from "../images/404background.jpg"
+import styled from "styled-components"
+
+const Block = styled.div`
+  display: grid;
+  place-items: center;
+  max-width: 340px;
+  margin: 4rem auto;
+`
 
 const NotFoundPage = () => (
   <Layout pageTitle="404 Not found">
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <LayoutBackground
+      background={background}
+      headerText={"Oops! 404: Not Found!"}
+    />
+    <Container>
+      <Block>
+        <h2 style={{ fontSize: "1.5rem" }}>
+          Sorry, we couldnt find the page you were looking for.
+        </h2>
+        <Button>
+          <Link to="/">Go home</Link>
+        </Button>
+      </Block>
+    </Container>
   </Layout>
 )
 
