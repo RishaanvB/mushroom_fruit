737f4d665f1fb302b6e5863e8d724d5dfb8ddfff
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-11-29 13:37:36 +0100

    add react helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,35 @@ import { useEffect } from "react"
 import "./layout.css"
 import MainHeader from "./mainheader"
 import Footer from "./footer"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children, pageTitle }) => {
-  useEffect(()=> document.title = `Mushroom Fruit | ${pageTitle}` || "Mushroom Fruit")
+  useEffect(
+    () => (document.title = `Mushroom Fruit | ${pageTitle}` || "Mushroom Fruit")
+  )
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="author" content="R van Beveren" />
+        <meta
+          name="description"
+          content="Mushroom related website providing reishi growkits and aims to provide information regarding reishi and other healthy mushroom related products."
+        />
+        <title>{`Mushroom Fruit | ${pageTitle}` || "Mushroom Fruit"}</title>
+        <meta
+          property="og:image"
+          content="https://mushroomfruit.gtsb.io/static/86551afd1dc1de0c26f3fca47e855a7d/d486f/logo.webp"
+        />
+        <meta
+          property="og:description"
+          content="Mushroom related website providing reishi growkits and aims to provide information regarding reishi and other healthy mushroom related products."
+        />
+        <meta
+          property="og:title"
+          content="MushroomFruit. Grow your own reishi!"
+        />
+      </Helmet>
       <MainHeader />
       <main>{children}</main>
       <Footer />
