8f2d5d7ea2c7e1d0aff015403763a929397ac426
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-12-02 20:01:03 +0100

    update meta data

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,22 +13,45 @@ const Layout = ({ children, pageTitle }) => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Mushroomfruit - Grow your own reishi!</title>
+        <meta name="title" content="Mushroomfruit - Grow your own reishi!" />
         <meta
           name="description"
           content="Mushroom related website providing reishi growkits and aims to provide information regarding reishi and other healthy mushroom related products."
         />
-        <title>{`Mushroom Fruit | ${pageTitle}` || "Mushroom Fruit"}</title>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mushroomfruit.com/" />
+        <meta property="og:url" content="https://mushroomfruit.nl/" />
         <meta
-          property="og:image"
-          content="https://mushroomfruit.gtsb.io/static/86551afd1dc1de0c26f3fca47e855a7d/d486f/logo.webp"
+          property="og:title"
+          content="Mushroomfruit - Grow your own reishi!"
         />
         <meta
           property="og:description"
           content="Mushroom related website providing reishi growkits and aims to provide information regarding reishi and other healthy mushroom related products."
         />
         <meta
-          property="og:title"
-          content="MushroomFruit. Grow your own reishi!"
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta
+          property="twitter:title"
+          content="Mushroomfruit - Grow your own reishi!"
+        />
+        <meta
+          property="twitter:description"
+          content="Mushroom related website providing reishi growkits and aims to provide information regarding reishi and other healthy mushroom related products."
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
         />
       </Helmet>
       <MainHeader />
