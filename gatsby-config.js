a50d7fef2e089b19c2332474fb39aa8feb219ca7
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2022-05-05 03:45:43 +0200

    fix: build issue, set ico to png

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         theme_color: `#F3BC77`,
         display: `minimal-ui`,
-        icon: `src/images/icon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
